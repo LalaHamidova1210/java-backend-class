02fd7ec7fd62776db6b3aa5d42dfa4cb80ddd4f4
LalaHamidova1210 <lalahamidova1210@gmail.com>
2024-10-26 16:27:55 +0400

    datatypes

--- src/main/java/az/turingacademy/Main.java
+++ src/main/java/az/turingacademy/Main.java
@@ -2,10 +2,20 @@ package az.turingacademy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        byte a= 1;
+        short b=2;
+        int c=3;
+        long d=4L;
+        float e=5.456789f;
+        double f= 6;
+        char g= 'L' ;
+        boolean h=true;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+
+       System.out.print (a);
+       System.out.println(a+"\n"+b);
+       System.out.printf("name %s, age %d \n", name, age);
+       System.out.printf("name, age %.2f\n:", 50.0 );
+
         }
     }
-}
